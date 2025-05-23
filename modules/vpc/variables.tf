ead0219f758c9c4bc9473f58ecce8ef823d0229a
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-05-23 16:02:22 +0530

    feat: create var values for vpc

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_name" {
+  type        = string
+  description = "name for the main vpc"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "range of ip addresses that in vpc"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "availability zones list"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "cidr values for public subnet"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "cidr values for private subnet"
+}
