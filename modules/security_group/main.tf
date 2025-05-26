ee33e00595b0a5d2267567c1fdc520f2d4ed3104
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-05-26 12:01:38 +0530

    feat : create ec2 computer security group

---
+++ modules/security_group/main.tf
@@ -0,0 +1,48 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = var.security_group_name
+  description = "security group for Ec2 isntance"
+  vpc_id      = var.vpc_id
+  tags = {
+    Name = var.security_group_name
+  }
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  description       = "allow SSH ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  description       = "allow HTTP ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "allow_https" {
+  type              = "ingress"
+  description       = "allow HTTPS ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  description       = "Allow All outbound traffic"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
---
+++ modules/security_group/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  type        = string
+  description = "vpc id for link sg"
+}
+
+variable "security_group_name" {
+  type        = string
+  description = "name for the ec2 security group"
+}
