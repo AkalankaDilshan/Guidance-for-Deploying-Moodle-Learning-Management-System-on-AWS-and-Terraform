dcc891ee32518d72d1ebe30d65180b38dda0810a
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-06-02 15:59:26 +0530

    fix : edit user data section

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -18,9 +18,15 @@ resource "aws_launch_template" "asg_lt" {
     associate_public_ip_address = true
     security_groups             = var.security_group_ids
   }
-  user_data = file("${path.module}/userdata_script.sh")
-
-
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from ASG instance $(hostname -f)</h1>" > /var/www/html/index.html
+              EOF
+  )
   tag_specifications {
     resource_type = "instance"
     tags = {
