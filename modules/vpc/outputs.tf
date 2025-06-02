e1cfb11f6ac3b49b02503bf8dcb62146401df61b
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-06-02 14:19:18 +0530

    feat: output subnets ids

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -4,3 +4,15 @@ output "vpc_id" {
 output "vpc_name" {
   value = var.vpc_name
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnet[*].id
+}
+
+output "private_subnet_ec2_ids" {
+  value = aws_subnet.private_subnet[*].id
+}
+
+output "private_subnet_data_ids" {
+  value = aws_subnet.private_data_subnet[*].id
+}
