70a28c924483ecee181051b3a1772501ab6eb658
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-06-02 16:07:11 +0530

    fix : launch template error

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,16 +8,13 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-resource "aws_launch_template" "asg_lt" {
-  name_prefix   = "asg-template-"
-  image_id      = data.aws_ami.amazon_linux_2.id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+resource "aws_launch_configuration" "asg_lt" {
+  name_prefix     = "asg-template-"
+  image_id        = data.aws_ami.amazon_linux_2.id
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = var.security_group_ids
 
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = var.security_group_ids
-  }
   user_data = base64encode(<<-EOF
               #!/bin/bash
               yum update -y
@@ -27,38 +24,26 @@ resource "aws_launch_template" "asg_lt" {
               echo "<h1>Hello from ASG instance $(hostname -f)</h1>" > /var/www/html/index.html
               EOF
   )
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "ASG-Instance"
-    }
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
 
 
 resource "aws_autoscaling_group" "example_asg" {
-  name_prefix         = "example-asg-"
-  max_size            = var.max_size
-  min_size            = var.min_size
-  desired_capacity    = var.desired_capacity
-  vpc_zone_identifier = var.subnet_ids
-
-  launch_template {
-    id      = aws_launch_template.asg_lt.id
-    version = "$Latest"
-  }
+  name_prefix          = "example-asg-"
+  max_size             = var.max_size
+  min_size             = var.min_size
+  desired_capacity     = var.desired_capacity
+  vpc_zone_identifier  = var.subnet_ids
+  launch_configuration = aws_launch_configuration.asg_lt.name
 
   health_check_type         = "ELB"
   health_check_grace_period = 300
 
   # Load balancing arn
   target_group_arns = var.target_group_arns
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
   tag {
     key                 = "Name"
     value               = "ASG-Instance"
